65c35e19b0b6842b83135e16dba5b32203931176
Blank <blank.xu@qq.com>
2020-08-10 14:45:28 +0800

    update travis

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # LeetCode-Mysql 
 
-[![Travis](https://img.shields.io/badge/mysql-v8.*-blue.svg)]()  [![Build Status](https://travis-ci.org/Blank-Xu/leetcode-mysql.svg?branch=master)](https://travis-ci.org/Blank-Xu/leetcode-mysql)  [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](LICENSE)
+[![Travis](https://img.shields.io/badge/mysql-v5.7-blue.svg)]()
+[![Build Status](https://travis-ci.org/Blank-Xu/leetcode-mysql.svg?branch=master)](https://travis-ci.org/Blank-Xu/leetcode-mysql)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](LICENSE)
 
 ---
 
--- test.sh
+++ test.sh
@@ -1,31 +1,37 @@
 #!/usr/bin/env bash
 
-PATH=$(cd `dirname $0`; pwd)
-DATABASE_NAME="leetcode-mysql-test"
+MYSQL=$1
+DATABASE_NAME=$2
+echo 'MYSQL_PATH: '${MYSQL}
+echo 'DATABASE_NAME: '${DATABASE_NAME}
 
+PATH=$(cd `dirname $0`; pwd)
 echo 'PATH: '${PATH}
 
+USERNAME="travis"
 
-echo "create database"
-mysql -e "CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME};"
-mysql -e "USE ${DATABASE_NAME};"
+MYSQL_CLIENT=`${MYSQL} -h 127.0.0.1 -P 3306 -u ${USERNAME} ${DATABASE_NAME}`
+echo 'MYSQL_CLIENT: '${MYSQL_CLIENT}
 
 #echo ''
-#mysql < ''
+#${MYSQL_CLIENT} < ''
 
 
+
+echo 'test start'
 
 echo '150-199---------------------------------------------'
 cd ${PATH}/150-199
 
 echo '175. Combine Two Tables.sql'
-mysql < '175. Combine Two Tables.sql'
+${MYSQL_CLIENT} < '175. Combine Two Tables.sql'
 
 echo '176. Second Highest Salary.sql'
-mysql < '176. Second Highest Salary.sql'
+${MYSQL_CLIENT} < '176. Second Highest Salary.sql'
 
 echo '181. Employees Earning More Than Their Managers.sql'
-mysql < '181. Employees Earning More Than Their Managers.sql'
+${MYSQL_CLIENT} < '181. Employees Earning More Than Their Managers.sql'
+
 
 
 
@@ -33,7 +39,8 @@ echo '550-599---------------------------------------------'
 cd ${PATH}/550-599
 
 echo '595. Big Countries.sql'
-mysql < '595. Big Countries.sql'
+${MYSQL_CLIENT} < '595. Big Countries.sql'
+
 
 
 
@@ -41,7 +48,7 @@ echo '600-649---------------------------------------------'
 cd ${PATH}/600-649
 
 echo '620. Not Boring Movies.sql'
-mysql < '620. Not Boring Movies.sql'
+${MYSQL_CLIENT} < '620. Not Boring Movies.sql'
 
 echo '627. Swap Salary.sql'
-mysql < '627. Swap Salary.sql'
+${MYSQL_CLIENT} < '627. Swap Salary.sql'
